d61f6f37a0bf20629192beb1c936f30692ee5d22
Ignas7206 <ignas7206@gmail.com>
2026-05-07 10:58:09 +0300

    Bump cache for quality update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tax-set-aside-v4";
+const CACHE_NAME = "tax-set-aside-v5";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
